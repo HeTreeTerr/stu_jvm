d72c1279f26f6b49e5519892e0f7226be720236c
hesensen <hesensen@hztianque.com>
2020-05-31 23:42:00 +0800

    堆的认知学习，以及堆内存溢出和内存诊断

---
+++ src/main/java/com/hss/demo/Demo1_8.java
@@ -0,0 +1,28 @@
+package com.hss.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 演示堆内存溢出
+ * java.lang.OutOfMemoryError: Java heap space
+ * -Xmx8m
+ */
+public class Demo1_8 {
+
+    public static void main(String[] args){
+        int i = 0;
+        try {
+            List<String> list = new ArrayList<>();
+            String a = "hello";
+            while (true) {
+                list.add(a);
+                a = a + a;
+                i++;
+            }
+        }catch (Throwable e){
+            e.printStackTrace();
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/main/java/com/hss/demo/Demo1_9.java
@@ -0,0 +1,19 @@
+package com.hss.demo;
+
+/**
+ * 演示堆内存
+ */
+public class Demo1_9 {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("1....");
+        Thread.sleep(30000);
+        byte[] array = new byte[1024 * 1024 * 10];//10MB
+        System.out.println("2....");
+        Thread.sleep(30000);
+        array = null;
+        System.gc();
+        System.out.println("3....");
+        Thread.sleep(1000000L);
+    }
+}
