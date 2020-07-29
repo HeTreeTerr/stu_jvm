52d08060906e8456a524ee4b2c3291fd676144e3
hesensen <hesensen@hztianque.com>
2020-07-29 20:26:52 +0800

    直接内存[释放]

---
+++ src/main/java/com/hss/demo/Demo1_24.java
@@ -0,0 +1,22 @@
+package com.hss.demo;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+public class Demo1_24 {
+
+    static int _1Gb = 1024 * 1024 * 1024;
+
+    /**
+     * -XX:+DisableExplicitGC 禁用显式的垃圾回收
+     */
+    public static void main(String[] args) throws IOException {
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(_1Gb);
+        System.out.println("分配完毕...");
+        System.in.read();
+        System.out.println("开始回收...");
+        byteBuffer = null;
+        System.gc();//显式的垃圾回收，full Gc
+        System.in.read();
+    }
+}
---
+++ src/main/java/com/hss/demo/Demo1_25.java
@@ -0,0 +1,33 @@
+package com.hss.demo;
+
+import sun.misc.Unsafe;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+
+public class Demo1_25 {
+    static int _1Gb = 1024 * 1024 * 1024;
+
+    public static void main(String[] args) throws IOException {
+        Unsafe unsafe = getUnsafe();
+        Long base = unsafe.allocateMemory(_1Gb);
+        unsafe.setMemory(base,_1Gb,(byte)0);
+        System.out.println("分配完毕");
+        System.in.read();
+
+        System.out.println("手动清空");
+        unsafe.freeMemory(base);
+        System.in.read();
+    }
+
+    public static Unsafe getUnsafe(){
+        try {
+            Field f = Unsafe.class.getDeclaredField("theUnsafe");
+            f.setAccessible(true);
+            Unsafe unsafe = (Unsafe)f.get(null);
+            return unsafe;
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
