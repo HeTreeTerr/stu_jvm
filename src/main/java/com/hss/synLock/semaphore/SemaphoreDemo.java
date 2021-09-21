b27d0aa9c102cc2dafb76e57d2b0e9d286956b36
‘HeTreeTerr’ <3110708879@qq.com>
2021-09-21 13:04:20 +0800

    java 锁信号量

---
+++ src/main/java/com/hss/synLock/semaphore/SemaphoreDemo.java
@@ -0,0 +1,30 @@
+package com.hss.synLock.semaphore;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 信号量
+ * 抢车位
+ */
+public class SemaphoreDemo {
+
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(3);
+
+        for (int i = 0; i < 6; i++) {
+            new Thread(()->{
+                try {
+                    semaphore.acquire();
+                    System.out.println(Thread.currentThread().getName() + "\t获得车位");
+                    TimeUnit.SECONDS.sleep(3);
+                    System.out.println(Thread.currentThread().getName() + "\t离开车位");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    semaphore.release();
+                }
+            },String.valueOf(i)).start();
+        }
+    }
+}
