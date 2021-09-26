b43bd340980528bb084bc15af7476c2c41be174b
hesensen <hess@erongdu.com>
2021-09-26 20:22:45 +0800

    synchronized和lock的比较 一个lock绑定多个condition，实现指定线程（或分组）执行顺序

---
+++ src/main/java/com/hss/synLock/enums/FrequencyEunm.java
@@ -0,0 +1,50 @@
+package com.hss.synLock.enums;
+
+public enum FrequencyEunm {
+    t1(1,5),
+    t2(2,10),
+    t3(3,15);
+
+    private int tid;
+
+    private int num;
+
+    FrequencyEunm(int tid,int num){
+        this.tid = tid;
+        this.num = num;
+    }
+
+    public int getTid() {
+        return tid;
+    }
+
+    public void setTid(int tid) {
+        this.tid = tid;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public static  int getNumByTid(int tid){
+        FrequencyEunm[] values = FrequencyEunm.values();
+        for(int i = 0; i < values.length; i++){
+            FrequencyEunm value = values[i];
+            if(value.tid == tid){
+                return value.num;
+            }
+        }
+        return 0;
+    }
+
+    /*public static void main(String[] args) {
+        System.out.println(FrequencyEunm.getNumByTid(1));
+        System.out.println(FrequencyEunm.getNumByTid(2));
+        System.out.println(FrequencyEunm.getNumByTid(3));
+        System.out.println(FrequencyEunm.getNumByTid(4));
+    }*/
+}
