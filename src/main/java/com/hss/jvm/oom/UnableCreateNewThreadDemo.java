c4addbbb0e56b88a6f83b508935855f69e1f6405
‘HeTreeTerr’ <3110708879@qq.com>
2021-10-18 00:21:38 +0800

    jvm 之 oom: unable to create new native thread

---
+++ src/main/java/com/hss/jvm/oom/UnableCreateNewThreadDemo.java
@@ -0,0 +1,25 @@
+package com.hss.jvm.oom;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * unable to create new native thread
+ * 不能创建新的线程
+ * 【谨慎运行，胡导致电脑死机】
+ */
+public class UnableCreateNewThreadDemo {
+
+    public static void main(String[] args) {
+//        无线循环
+        for (int i = 1; ; i++) {
+//            创建新的线程，同时阻塞
+            new Thread(()->{
+                try {
+                    TimeUnit.SECONDS.sleep(Integer.MAX_VALUE);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            },String.valueOf(i)).start();
+        }
+    }
+}
