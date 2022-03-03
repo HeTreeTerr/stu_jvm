9d8e8080fec2b43749229a9995a8427179185118
hesensen <hess@erongdu.com>
2022-03-03 14:18:02 +0800

    java 基本运算符

--- src/main/java/com/hss/operator/BasicOperator.java
+++ src/main/java/com/hss/operator/BasicOperator.java
@@ -18,6 +18,12 @@ public class BasicOperator {
         // 与运算(|)
         //basicOperator.andOperation();
 
+        // 异或运算符(^)
+        //basicOperator.xorOperation();
+
+        // 取反运算符(~)
+        //basicOperator.negateOperation();
+
         // 左移(<<)
         //basicOperator.shiftLeft();
 
@@ -65,6 +71,33 @@ public class BasicOperator {
         System.out.println("\n=======<<=======");
         int result = a << 2;
         System.out.println("a=" + Integer.toBinaryString(a));
+        System.out.println("c=" + Integer.toBinaryString(result));
+    }
+
+    /**
+     * 取反运算符(~)
+     * 二进制数，所有位取反
+     */
+    public void negateOperation(){
+        System.out.println("\n=======~=======");
+        int result = ~a;
+        /*
+        int 类型，占据32bit(位),4byte(字节)
+         */
+        System.out.println("a=00000000000000000000000000" + Integer.toBinaryString(a));
+        System.out.println("c=" + Integer.toBinaryString(result));
+    }
+
+    /**
+     * 异或运算符(^)
+     * 二进制数，两个相应位为“异”（值不同），则该位结果为1
+     * 否则为0
+     */
+    public void xorOperation(){
+        System.out.println("\n=======^=======");
+        int result = a ^ (b - 1);
+        System.out.println("a=" + Integer.toBinaryString(a));
+        System.out.println("b=00" + Integer.toBinaryString(b-1));
         System.out.println("c=" + Integer.toBinaryString(result));
     }
 
