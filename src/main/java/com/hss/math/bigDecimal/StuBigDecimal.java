e6d09e3a5036644813870b18548f17c11a2ad9c3
hesensen <hess@erongdu.com>
2022-06-09 16:17:35 +0800

    BigDecimal 操作

--- src/main/java/com/hss/math/bigDecimal/StuBigDecimal.java
+++ src/main/java/com/hss/math/bigDecimal/StuBigDecimal.java
@@ -5,6 +5,18 @@ import java.math.BigDecimal;
 public class StuBigDecimal {
 
     public static void main(String[] args) {
+
+        //费用计算
+//        countPremium();
+
+        //比较大小
+        comparisonOfSize();
+    }
+
+    /**
+     * 费用计算
+     */
+    public static void countPremium(){
         //保额(amount)
         BigDecimal amount = new BigDecimal("12344.00");
         //费率(fee)(单位:%)
@@ -13,6 +25,22 @@ public class StuBigDecimal {
         BigDecimal calAmt = amount.multiply(fee).divide(new BigDecimal(100))
                 .setScale(2, BigDecimal.ROUND_DOWN);
 
-        System.out.println(calAmt);
+        System.out.println("费用：" + calAmt);
+    }
+
+    /**
+     * 比较大小
+     */
+    public static void comparisonOfSize(){
+        BigDecimal param1 = new BigDecimal(800);
+        BigDecimal param2 = new BigDecimal(800);
+        int res = param1.compareTo(param2);
+        if(res == 1){
+            System.out.println("p1 大于 p2");
+        }else if (res == 0){
+            System.out.println("p1 等于 p2");
+        }else if (res == -1){
+            System.out.println("p1 小于 p2");
+        }
     }
 }
