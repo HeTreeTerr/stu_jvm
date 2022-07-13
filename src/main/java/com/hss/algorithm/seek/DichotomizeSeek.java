d53688d2ce9d63f8851622ba6f5f64fa49e709ca
‘HeTreeTerr’ <3110708879@qq.com>
2022-07-13 22:18:04 +0800

    二分查找

--- src/main/java/com/hss/algorithm/seek/DichotomizeSeek.java
+++ src/main/java/com/hss/algorithm/seek/DichotomizeSeek.java
@@ -9,15 +9,14 @@ public class DichotomizeSeek {
     public static void main(String[] args) {
 
         int i = 55;
-        int[] arry = {1,5,19,20,55};
+        int[] arry = {1,5,19,20,55,65,75};
 
         int leftIndex = 0;
-        int rightIndex = arry.length;
+        int rightIndex = arry.length - 1;
         int res = -1;
-        //todo...
         while (leftIndex <= rightIndex){
             //得到中间位置
-            int middle = (rightIndex - leftIndex) / 2;
+            int middle = (rightIndex + leftIndex) / 2;
             System.out.println(leftIndex + "--" + rightIndex + "--" + middle);
             if(arry[middle] == i){//匹配到值
                 res = middle;
