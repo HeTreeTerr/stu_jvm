f1f4bf7f55f25003a2b5c8f56e5622dea005e1cb
hesensen <hess@erongdu.com>
2022-07-29 17:38:02 +0800

    插入排序

---
+++ src/main/java/com/hss/algorithm/sequence/DirectInsertionSort.java
@@ -0,0 +1,18 @@
+package com.hss.algorithm.sequence;
+
+/**
+ * <p>
+ * 直接插入排序
+ * </p>
+ *
+ * @author Hss
+ * @date 2022-07-29
+ */
+public class DirectInsertionSort {
+
+    public static void main(String[] args) {
+        int[] numArr = {78, 15, 24, 66, 55, 33};
+
+        System.out.println(numArr);
+    }
+}
