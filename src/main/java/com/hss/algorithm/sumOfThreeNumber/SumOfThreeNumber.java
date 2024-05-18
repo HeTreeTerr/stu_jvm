18e82d45156e355dad451004900c0906476bbc61
HeTreeTerr <3110708879@qq.com>
2024-05-18 17:05:28 +0800

    算法：三数之和02

--- src/main/java/com/hss/algorithm/sumOfThreeNumber/SumOfThreeNumber.java
+++ src/main/java/com/hss/algorithm/sumOfThreeNumber/SumOfThreeNumber.java
@@ -18,11 +18,49 @@ public class SumOfThreeNumber {
 
         int[] nums = new int[] {-1, -1, 0, 9, 2, 0, 0, 0};
         //int[] nums = new int[] {-4,-2,1,-5,-4,-4,4,-2,0,4,0,-2,3,1,-5,0};
-        System.out.println(threeSum01(nums).toString());
+        //System.out.println(threeSum01(nums).toString());
+
+        System.out.println(threeSum02(nums).toString());
+    }
+
+    /**
+     * 方法二：暴力法
+     * 时间复杂度：O（n^2）
+     * 结论：不可行，map结构，存在可能性丢失、可能性重复等问题
+     * @param nums
+     * @return
+     */
+    public static List<List<Integer>> threeSum02(int[] nums) {
+        //定义返回list
+        List<List<Integer>> resList = new ArrayList<>();
+        //临时空间
+        Map<Integer,List<Integer>> tempList = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            Integer thatNum = -nums[i];
+            if(tempList.containsKey(thatNum)){
+                List<Integer> itemList = new ArrayList<>(tempList.get(thatNum));
+                itemList.add(nums[i]);
+                resList.add(itemList);
+                continue;
+            }
+            if(i < nums.length-1){
+                //System.out.println(nums[i] + "------------");
+                for (int j = i+1; j < nums.length; j++){
+                    //System.out.println(nums[i] + "--" + nums[j]);
+                    Integer newNum = nums[i] + nums[j];
+                    if(!tempList.containsKey(newNum)){
+                        List<Integer> itemList = Arrays.asList(nums[i], nums[j]);
+                        tempList.put(newNum,itemList);
+                    }
+                }
+            }
+        }
+        return resList;
     }
 
     /**
      * 方法一：暴力法
+     * 时间复杂度：O（n^3）
      * @param nums
      * @return
      */
