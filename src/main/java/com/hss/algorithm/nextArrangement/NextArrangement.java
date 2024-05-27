7ec04c29892b5a441861489a76ca7c20a2c8c58c
HeTreeTerr <3110708879@qq.com>
2024-05-27 21:36:33 +0800

    临时文件移除

---
+++ src/main/java/com/hss/algorithm/nextArrangement/NextArrangement.java
@@ -0,0 +1,18 @@
+package com.hss.algorithm.nextArrangement;
+
+/**
+ * <p>
+ * 下一个排列
+ * </p>
+ *
+ * @author Hss
+ * @date 2024-05-19
+ */
+public class NextArrangement {
+
+    public static void main(String[] args) {
+
+        int[] nums = new int[]{1,3,8,7,6,2};
+        //todo...
+    }
+}
